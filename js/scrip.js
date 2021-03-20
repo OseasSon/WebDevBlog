03c513dcf54906ca0897e02f53b780ad912ea4fe
Oseas Filho <oseas.filho@hotmail.com>
2021-03-20 23:05:25 +0000

    My first web project!
    Client-side CA1

---
+++ js/scrip.js
@@ -0,0 +1,13 @@
+alert('Welcome to this page, enjoy your reading!');
+
+//the following funcions have been based on examples from https://www.w3schools.com/ 
+
+function turnOn() {
+    document.getElementById("myImage").src='img/pic_bulbon.gif'; 
+}
+    
+function turnOff() {
+    document.getElementById("myImage").src='img/pic_bulboff.gif';  
+}
+    
+    
